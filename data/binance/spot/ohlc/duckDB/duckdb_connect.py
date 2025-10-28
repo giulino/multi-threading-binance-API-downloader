61ef9453a5e5eb0389479753fd70b5402626944b
giulino <giulio.mannini1@gmail.com>
2025-10-28 23:03:11 +0100

    added data_layer repo

--- data/binance/spot/ohlc/duckDB/duckdb_connect.py
+++
@@ -1,7 +0,0 @@
-import duckdb
-import os 
-
-def connect_spot(file_path):
-    path = os.path.expanduser(file_path)
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    return duckdb.connect(path)
